3abb33e72e15ffc76f4afeb7f2fb55dcdeca8810
Malak_Slimene <120478218+SLIMENE-Malak@users.noreply.github.com>
2023-03-26 00:52:20 +0100

    Updated notifications.
    Updated notifications.

--- feed.js
+++ feed.js
@@ -3,7 +3,7 @@ var exitButton = document.getElementById('exit');
 
 
 searchButton.addEventListener("click", openSearch);
-exitButton.addEventListener("click", closeSearch)
+exitButton.addEventListener("click", closeSearch);
 
 function openSearch() {
     document.getElementsByClassName("search-window")[0].style.display = "flex";
@@ -12,5 +12,20 @@ function openSearch() {
   
 function closeSearch() {
     document.getElementsByClassName("search-window")[0].style.display = "none";
+    document.getElementsByClassName("containter")[0].style.filter = "none";
+}
+var NotificationsButton = document.getElementsByClassName('notifications')[0];
+var exitButton1 = document.getElementById('exit1');
+
+NotificationsButton.addEventListener("click", openNotifications);
+exitButton1.addEventListener("click", closeNotifications);
+
+function openNotifications() {
+    document.getElementsByClassName("Notifications")[0].style.display = "flex";
+    document.getElementsByClassName("containter")[0].style.filter = "blur(5px)";
+}
+  
+function closeNotifications() {
+    document.getElementsByClassName("Notifications")[0].style.display = "none";
     document.getElementsByClassName("containter")[0].style.filter = "none";
 }
