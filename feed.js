4cf73e1d1c73a501d6d6aa56a121e4a4b75ca56f
Malak_Slimene <120478218+SLIMENE-Malak@users.noreply.github.com>
2023-03-27 00:32:47 +0200

    Added functionality to the "more" window.
    Added functionality to the "more" window.

--- feed.js
+++ feed.js
@@ -85,3 +85,40 @@ for(i=0; i<hearts.length; i++){
         l = `<i class='bx bxs-heart' style='color:#ff0000'  ></i>`;
     });
 }*/
+var moreButton = document.getElementsByClassName('more')[0];
+var exitButton1 = document.getElementById('exit');
+
+moreButton.addEventListener("click", openmore);
+exitButton1.addEventListener("click", closemore);
+
+function openmore() {
+    document.getElementsByClassName("pluss")[0].style.display = "flex";
+    document.getElementsByClassName("containter")[0].style.filter = "blur(5px)";
+    var text = document.getElementsByTagName("p");
+    for(i=0; i<text.length; i++){
+        text[i].style.userSelect = "none";
+    }
+    var anchorTags = document.getElementsByTagName("a");
+    for(i=0; i<anchorTags.length; i++){
+       if(anchorTags[i].id!="poweroff"){
+        
+        anchorTags[i].style.pointerEvents = "none";
+        anchorTags[i].style.userSelect = "none";
+       }
+       
+    }
+}
+  
+function closemore() {
+    document.getElementsByClassName("pluss")[0].style.display = "none";
+    document.getElementsByClassName("containter")[0].style.filter = "none";
+    var text = document.getElementsByTagName("p");
+    for(i=0; i<text.length; i++){
+        text[i].style.userSelect = "auto";
+    }
+    var anchorTags = document.getElementsByTagName("a");
+    for(i=0; i<anchorTags.length; i++){
+        anchorTags[i].style.pointerEvents = "auto";
+        anchorTags[i].style.userSelect = "auto";
+    }
+}
