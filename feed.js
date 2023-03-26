1130a5bb4b41d7b96ffb3c9281b056b944180dd7
Firas Riahi <120566908+riahifiras@users.noreply.github.com>
2023-03-26 16:19:04 +0100

    Major changes to feed page.
    made the feed page more interactive and now it looks and feels better
    the website is now mostly compatible with small screens

--- feed.js
+++ feed.js
@@ -1,5 +1,5 @@
 var searchButton = document.getElementsByClassName('search')[0];
-var exitButton = document.getElementById('exit');
+var exitButton = document.getElementsByClassName('exit')[1];
 
 
 searchButton.addEventListener("click", openSearch);
@@ -8,14 +8,40 @@ exitButton.addEventListener("click", closeSearch);
 function openSearch() {
     document.getElementsByClassName("search-window")[0].style.display = "flex";
     document.getElementsByClassName("containter")[0].style.filter = "blur(5px)";
+    var text = document.getElementsByTagName("p");
+    for(i=0; i<text.length; i++){
+        text[i].style.userSelect = "none";
+    }
+    var anchorTags = document.getElementsByTagName("a");
+    for(i=0; i<anchorTags.length; i++){
+        anchorTags[i].style.userSelect = "none";
+        anchorTags[i].style.pointerEvents = "none";
+    }
+    /*var buttons = document.getElementsByTagName("button");
+    for(i=0; i<buttons.length; i++){
+        if(buttons[i].className = "exit"){
+            continue;
+        }
+        buttons[i].disabled = true;
+    }*/
 }
   
 function closeSearch() {
     document.getElementsByClassName("search-window")[0].style.display = "none";
     document.getElementsByClassName("containter")[0].style.filter = "none";
+    var text = document.getElementsByTagName("p");
+    for(i=0; i<text.length; i++){
+        text[i].style.userSelect = "auto";
+    }
+    var anchorTags = document.getElementsByTagName("a");
+    for(i=0; i<anchorTags.length; i++){
+        anchorTags[i].style.pointerEvents = "auto";
+        anchorTags[i].style.userSelect = "auto";
+    }
+    
 }
 var NotificationsButton = document.getElementsByClassName('notifications')[0];
-var exitButton1 = document.getElementById('exit1');
+var exitButton1 = document.getElementsByClassName('exit')[0];
 
 NotificationsButton.addEventListener("click", openNotifications);
 exitButton1.addEventListener("click", closeNotifications);
@@ -23,9 +49,39 @@ exitButton1.addEventListener("click", closeNotifications);
 function openNotifications() {
     document.getElementsByClassName("Notifications")[0].style.display = "flex";
     document.getElementsByClassName("containter")[0].style.filter = "blur(5px)";
+    var text = document.getElementsByTagName("p");
+    for(i=0; i<text.length; i++){
+        text[i].style.userSelect = "none";
+    }
+    var anchorTags = document.getElementsByTagName("a");
+    for(i=0; i<anchorTags.length; i++){
+        anchorTags[i].style.pointerEvents = "none";
+        anchorTags[i].style.userSelect = "none";
+    }
 }
   
 function closeNotifications() {
     document.getElementsByClassName("Notifications")[0].style.display = "none";
     document.getElementsByClassName("containter")[0].style.filter = "none";
+    var text = document.getElementsByTagName("p");
+    for(i=0; i<text.length; i++){
+        text[i].style.userSelect = "auto";
+    }
+    var anchorTags = document.getElementsByTagName("a");
+    for(i=0; i<anchorTags.length; i++){
+        anchorTags[i].style.pointerEvents = "auto";
+        anchorTags[i].style.userSelect = "auto";
+    }
 }
+/*
+var hearts = document.getElementsByClassName("heart");
+for(i=0; i<hearts.length; i++){
+    var l = hearts[i].innerHTML;
+    hearts[i].addEventListener("mouseover", function(){
+        l = `<i class='bx bxs-heart' style='color:#ff0000'  ></i>`;
+    });
+    hearts[i].addEventListener("click", function(){
+        console.log(l);
+        l = `<i class='bx bxs-heart' style='color:#ff0000'  ></i>`;
+    });
+}*/
