d306a6733a79c03f24c130955e417b4b78e1b5d3
Firas Riahi <120566908+riahifiras@users.noreply.github.com>
2023-04-05 17:02:46 +0100

    Add mor dark mode buttons.

--- dark.js
+++ dark.js
@@ -19,3 +19,33 @@ else{
     document.documentElement.style.setProperty('--color6', '#9c6644');
     document.documentElement.style.setProperty('--black', '#232b2b');
 }
+
+const modeBtn = document.getElementsByClassName("mode")[0];
+modeBtn.addEventListener("click", changeMode);
+
+function changeMode(){
+    
+    let mode = localStorage.getItem("mode");
+    if(mode == "light"){
+        localStorage.setItem("mode", "dark");
+        document.documentElement.style.setProperty('--white', '#000000');
+        document.documentElement.style.setProperty('--color1', '#2d241f');
+        document.documentElement.style.setProperty('--color2', '#3d3328');
+        document.documentElement.style.setProperty('--color3', '#514739');
+        document.documentElement.style.setProperty('--color4', '#6a5b4f');
+        document.documentElement.style.setProperty('--color5', '#8c7b66');
+        document.documentElement.style.setProperty('--color6', '#bfaa8f');
+        document.documentElement.style.setProperty('--black', '#ffffff');
+    }
+    else{
+        localStorage.setItem("mode", "light");
+        document.documentElement.style.setProperty('--white', '#ffffff');
+        document.documentElement.style.setProperty('--color1', '#ede0d4');
+        document.documentElement.style.setProperty('--color2', '#e6ccb2');
+        document.documentElement.style.setProperty('--color3', '#ddb892');
+        document.documentElement.style.setProperty('--color4', '#b08968');
+        document.documentElement.style.setProperty('--color5', '#7f5539');
+        document.documentElement.style.setProperty('--color6', '#9c6644');
+        document.documentElement.style.setProperty('--black', '#232b2b');
+    }
+}
